532a1f4a3e5899103961dbd9014d6831850087cb
borchand <andersen_sebastian@yahoo.dk>
2022-09-18 16:22:25 +0200

    Dark and light color theme

--- src/Sites/layout.js
+++ src/Sites/layout.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
 import "./layout.css";
+import "../theme.css";
 import { VscThreeBars } from 'react-icons/vsc';
-
+import useLocalStorage from 'use-local-storage'
 
 const Layout = () => {
   const [shouldShowMenu, setShouldShowMenu] = React.useState(false)
@@ -10,19 +11,26 @@ const Layout = () => {
   
   const showMenu = () => setShouldShowMenu(!shouldShowMenu)
 
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+  const switchTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  }
   return (
-    <>
+    <div data-theme={theme}>
       <VscThreeBars className="menu-icon" size={25} onClick={() => showMenu()}/>
       {shouldShowMenu ? 
         <div className="menu show-menu" onClick={() => showMenu()}>
           <Link to="/">Home</Link>
           <Link to="/test">Test</Link>
+          <button onClick={switchTheme}>Switch theme</button>
         </div>
       : null}
       
 
       <Outlet />
-    </>
+    </div>
   )
 };
 
--- src/Sites/projects.js
+++ src/Sites/projects.js
@@ -1,7 +1,15 @@
 import React from "react";
+import useLocalStorage from 'use-local-storage'
 
 export default function Test() {
+
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
   return (
-    <p>test</p>
+    <div className="theme">
+      
+      <p>test</p>
+    </div>
   );
 }
